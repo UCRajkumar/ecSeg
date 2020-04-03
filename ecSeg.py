662b4be413af9852ec955611021579188747c741
URajkumar <UCRajkumar@users.noreply.github.com>
2020-04-03 05:26:44 -0700

    Update ecSeg.py

--- ecSeg.py
+++ ecSeg.py
@@ -3,10 +3,9 @@
 #Email: urajkuma@eng.ucsd.edu
 #Loads in trained model and produces segmentation maps of images in folder
 
-import os
 from predict import predict
 from keras.models import Model, load_model
-import sys, getopt
+import sys, getopt, os, csv
 from keras import backend as K
 
 if sys.version_info[0] < 3:
@@ -46,13 +45,18 @@ def main(argv):
     
     print("Loading in trained model...")
     model = load_model(model_name, compile=False) #load model
-    print("Model loaded.")
+    img_names = []
+    tot_ec = []
     for f in os.listdir(inputfile): #get all images in path
         ext = os.path.splitext(f)[1]
-        if ext.lower() == '.tif':
-          print('Segmenting',f)
-          predict(model, inputfile, (f))
+        if ext.lower() == '.tif' or ext.lower()=='.tiff':
+            print('Segmenting',f)
+            img_name, num_ecDNA = predict(model, inputfile, (f))
+            img_names.append(img_name); tot_ec.append(num_ecDNA)            
     print("Successfully exited...")
+    with open(inputfile+'/num_ecDNAs.csv', 'w') as f:
+        writer = csv.writer(f, delimiter=',')    
+        writer.writerows(zip(img_names, tot_ec))
     K.clear_session()
 if __name__ == "__main__":
    main(sys.argv[1:])
