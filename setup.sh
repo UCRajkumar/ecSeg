6b1abbe15520ad14736b0932835e98c36e3afe13
ucrajkumar <urajkuma@eng.ucsd.edu>
2020-10-09 15:01:10 -0700

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo "Installing required libraries"
-# conda create --name ecseg --file requirements.txt
-# conda activate ecseg
+conda create --name ecseg --file requirements.txt
+conda activate ecseg
 
 echo "Successfully setup ecseg"
