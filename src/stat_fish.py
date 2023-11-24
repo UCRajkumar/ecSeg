45ad0963bca0e4c12a3fb412bd6a2ec3d9190272
Gino Prasad <giprasad@ucsd.edu>
2023-11-24 10:29:19 -0800

    Fixed uint8 subtraction bug.

--- src/stat_fish.py
+++ src/stat_fish.py
@@ -258,9 +258,11 @@ def main(argv):
                 centroids.append(str(int(center[0])) + '_' + str(int(center[1])))
                 names.append(path_split[-1][:-4])
                 
-                green_red_splice = (fish[0] // 255) * (fish[1] // 255)
+                green_red_splice = ((fish[0]) * (fish[1] / 255))
                 blob_count = count_blobs(green_red_splice, cell_seg, min_cc_size)
-                green_red_pixels.append((green_red_splice * cell_seg).sum())
+                fish_pixels = (green_red_splice * cell_seg).sum() / 255
+                assert fish_pixels == int(fish_pixels)
+                green_red_pixels.append(int(fish_pixels))
                 green_red_blobs.append(blob_count)
 
             df['image_name'] = np.array(names)
